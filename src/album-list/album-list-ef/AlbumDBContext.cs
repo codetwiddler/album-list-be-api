fa72fd52dfdf10c2a748b2bdf58cf656f6db2c16
codetwiddler <codetwiddler>
2024-01-07 11:17:01 -0700

    Setting up dbcontext and repository for Album

---
+++ src/album-list/album-list-ef/AlbumDBContext.cs
@@ -0,0 +1,15 @@
+﻿using album_list_model;
+using Microsoft.EntityFrameworkCore;
+
+namespace album_list_ef
+{
+    public class AlbumDbContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("AlbumDatabase");
+        }
+    }
+}
---
+++ src/album-list/album-list-ef/AlbumRepository.cs
@@ -0,0 +1,59 @@
+﻿using album_list_model;
+
+namespace album_list_ef
+{
+
+
+    public class AlbumRepository
+    {
+        private readonly AlbumDbContext _dbContext;
+
+        public AlbumRepository(AlbumDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void AddAlbum(Album album)
+        {
+            _dbContext.Albums.Add(album);
+            _dbContext.SaveChanges();
+        }
+
+        public Album GetAlbumById(int id)
+        {
+            return _dbContext.Albums.Find(id);
+        }
+
+        public IQueryable<Album> GetAllAlbums()
+        {
+            return _dbContext.Albums.AsQueryable();
+        }
+
+        public void UpdateAlbum(Album updatedAlbum)
+        {
+            var existingAlbum = _dbContext.Albums.Find(updatedAlbum.Id);
+
+            if (existingAlbum != null)
+            {
+                existingAlbum.Title = updatedAlbum.Title;
+                existingAlbum.Artist = updatedAlbum.Artist;
+                existingAlbum.ReleaseYear = updatedAlbum.ReleaseYear;
+                existingAlbum.Genre = updatedAlbum.Genre;
+
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public void DeleteAlbum(int id)
+        {
+            var albumToDelete = _dbContext.Albums.Find(id);
+
+            if (albumToDelete != null)
+            {
+                _dbContext.Albums.Remove(albumToDelete);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+
+}
