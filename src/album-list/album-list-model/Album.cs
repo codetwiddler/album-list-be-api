c012cccaf505ec1b972bf66f6b7925f7407e23c5
codetwiddler <codetwiddler>
2024-01-07 14:22:13 -0700

    Setting data type for database columns

--- src/album-list/album-list-model/Album.cs
+++ src/album-list/album-list-model/Album.cs
@@ -1,11 +1,26 @@
-﻿namespace album_list_model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace album_list_model
 {
     public class Album
     {
+        [Key]
         public int Id { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(50)]
         public string Title { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(100)]
         public string Artist { get; set; }
+
+        [Column(TypeName = "SMALLINT")]        
         public int ReleaseYear { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(50)]
         public string Genre { get; set; }
     }
 }
