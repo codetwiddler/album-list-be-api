20887842b576c8012b4b60b8a3b19e020a537a4c
David R. Smith <codetwiddler@gmail.com>
2024-01-09 20:04:38 -0700

    Revert "remove id because it is handled by DB and adjust formatting" because I missed my DTO entirely!
    This reverts commit 05f4cc0b3d0d61490025060ff93183bee2b8d8b1.

--- src/album-list/album-list-business/Command/CreateAlbumCommand.cs
+++ src/album-list/album-list-business/Command/CreateAlbumCommand.cs
@@ -6,19 +6,20 @@ namespace album_list_business.Command
 {
     public class CreateAlbumCommand : IRequest<Result<AlbumResponse>>
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Artist { get; set; }
-        public int    ReleaseYear { get; set; }
+        public int ReleaseYear { get; set; }
         public string Genre { get; set; }
-        public int    Rating { get; set; }
+        public int Rating { get; set; }
 
         public CreateAlbumCommand(string title, string artist, int releaseYear, string genre, int rating)
         {
-            Title       = title;
-            Artist      = artist;
+            Title = title;
+            Artist = artist;
             ReleaseYear = releaseYear;
-            Genre       = genre;
-            Rating      = rating;
+            Genre = genre;
+            Rating = rating;
         }
     }
 }
