4504a2f1cd477326c6fe489d2c0018590e2392e4
David R. Smith <codetwiddler@gmail.com>
2024-01-09 19:58:09 -0700

    formatting and use of dto

--- src/album-list/album-list-business/Command/UpdateAlbumCommand.cs
+++ src/album-list/album-list-business/Command/UpdateAlbumCommand.cs
@@ -6,21 +6,21 @@ namespace album_list_business.Command
 {
     public class UpdateAlbumCommand : IRequest<Result<AlbumResponse>>
     {
-        public int Id { get; set; }
+        public int    Id { get; set; }
         public string Title { get; set; }
         public string Artist { get; set; }
-        public int ReleaseYear { get; set; }
+        public int    ReleaseYear { get; set; }
         public string Genre { get; set; }
-        public int Rating { get; set; }
+        public int    Rating { get; set; }
 
-        public UpdateAlbumCommand(int id, string title, string artist, int releaseYear, string genre, int rating)
+        public UpdateAlbumCommand(int id, UpdateAlbumDto updateAlbumDto)
         {
-            Id = id;
-            Title = title;
-            Artist = artist;
-            ReleaseYear = releaseYear;
-            Genre = genre;
-            Rating = rating;
+            Id          = id;
+            Title       = updateAlbumDto.Title;
+            Artist      = updateAlbumDto.Artist;
+            ReleaseYear = updateAlbumDto.ReleaseYear;
+            Genre       = updateAlbumDto.Genre;
+            Rating      = updateAlbumDto.Rating;
         }
     }
 }
