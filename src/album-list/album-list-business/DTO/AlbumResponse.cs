df4ee7061b2d2a8f501df38a923675bac544a591
David R. Smith <codetwiddler@gmail.com>
2024-01-09 19:59:28 -0700

    formatting and commentary only

--- src/album-list/album-list-business/DTO/AlbumResponse.cs
+++ src/album-list/album-list-business/DTO/AlbumResponse.cs
@@ -2,11 +2,15 @@
 {
     public class AlbumResponse
     {
-        public int Id { get; set; }
+        //Setting up a DTO object for a response is wise in case we decide there
+        //are other data we wish to include in the response. For instance,
+        //we may later decide to paginate all of our API's results and this
+        //type of DTO would provide an easy avenue to achieve that goal
+        public int    Id { get; set; }
         public string Title { get; set; }
         public string Artist { get; set; }
-        public int ReleaseYear { get; set; }
+        public int    ReleaseYear { get; set; }
         public string Genre { get; set; }
-        public int? Rating { get; set; }
+        public int    Rating { get; set; }
     }
 }
