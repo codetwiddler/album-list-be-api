b1d60197517ce6b17cb360d320bd9a876cb2db2e
David R. Smith <codetwiddler@gmail.com>
2024-01-09 19:34:11 -0700

    Add some DTOs to clean up command structures

---
+++ src/album-list/album-list-business/DTO/CreateAlbumDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace album_list_business.DTO
+{
+    public class CreateAlbumDto
+    {
+        //We aren't doing it in this project, but setting default values in our DTOs
+        //can be convenient if we have endpoint considerations (like using Automapper)
+        //or, if we're establishing another barrier or separation of concerns between
+        //external interfaces and internal logic. It could also provide flexibility
+        //with more complex data types
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int    ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public int    Rating { get; set; }
+    }
+}
