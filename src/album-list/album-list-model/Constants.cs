89a761f29c9915503f2222fa77706dfec0913ec3
David R. Smith <codetwiddler@gmail.com>
2024-01-09 19:34:39 -0700

    Add some constants for initialization values and "localization" options

---
+++ src/album-list/album-list-model/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace album_list_model
+{
+    internal class Constants
+    {
+        public const string STR_UNKNOWN_ARTIST = "Unknown Artist";
+        public const string STR_UNKNOWN_TITLE = "Unknown Title";
+        public const string STR_UNSPECIFIED = "Unspecified";
+    }
+}
